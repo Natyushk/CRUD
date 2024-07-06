dd8e092be2e3411684e9df0fe5f17be3ba04be55
Natyushk <136663795+Natyushk@users.noreply.github.com>
2024-07-05 23:24:06 -0500

    Create endpoint for "find one"

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { Book } from './book.schema';
 
@@ -13,6 +13,10 @@ export class BooksController {
   @Get()
   async findAll(): Promise<Book[]> {
     return this.booksService.findAll();
+  }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Book> {
+    return this.booksService.findOne(id);
   }
 }
  
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Book } from './book.schema';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class BooksService {
@@ -13,5 +14,8 @@ export class BooksService {
   }
   async findAll(): Promise<Book[]> {
     return this.bookModel.find().exec();
+  }
+  async findOne(id: string): Promise<Book> {
+    return this.bookModel.findById(new ObjectId(id)).exec();
   }
 }
